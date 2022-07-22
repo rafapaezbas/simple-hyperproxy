377a9d8a3b519f9cb66866d1a98d925f8cfc2d60
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-07-22 13:04:27 +0100

    correct arg indices

--- bin.mjs
+++ bin.mjs
@@ -2,19 +2,19 @@
 
 import SimpleHyperProxy from './index.js'
 
-const args = process.argv.slice(2)
-const command = args[2]
-
+const command = process.argv[2]
 const proxy = new SimpleHyperProxy()
 
+const args = process.argv.slice(3)
+
 if (command === 'expose') {
-  const port = args[3]
-  const seed = args[4]
+  const port = args[0]
+  const seed = args[1]
   const key = await proxy.expose(port, seed)
   console.log(`Exposed localhost:${port} in key ${key.toString('hex')}`)
 } else if (command === 'bind') {
-  const key = Buffer.from(args[3], 'hex')
-  const port = await proxy.bind(key, args[4])
+  const key = Buffer.from(args[0], 'hex')
+  const port = await proxy.bind(key, args[1])
   console.log(`Binded ${key.toString('hex')} to port ${port}`)
 } else {
   printHelp()
